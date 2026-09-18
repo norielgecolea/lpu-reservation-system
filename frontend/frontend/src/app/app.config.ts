91ccf5933d8918cafae51d22ca240bc1f5269514
Noriel Gecolea <angelgecolea23@gmail.com>
2026-09-18 08:41:41 +0800

    optimization

--- frontend/frontend/src/app/app.config.ts
+++ frontend/frontend/src/app/app.config.ts
@@ -1,15 +1,16 @@
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withPreloading } from '@angular/router';
 
 import { authInterceptor } from './core/auth/auth.interceptor';
+import { IdlePreloadingStrategy } from './core/idle-preloading.strategy';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withPreloading(IdlePreloadingStrategy)),
     provideClientHydration(),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
   ],
